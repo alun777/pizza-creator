0d2ad213933a4332041dbce0e418adb268cc0e8d
ChaoYIN <yinchao428@hotmail.com>
2019-08-06 21:21:14 +1000

    Updates

---
+++ precache-manifest.7274efb481d4abe93cdc6b9e919ba645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50d32dd539bddfa92e7be3ef8b923f9",
+    "url": "/pizza-creator/index.html"
+  },
+  {
+    "revision": "37ba4a1dc11123eaa106",
+    "url": "/pizza-creator/static/css/main.acd82de7.chunk.css"
+  },
+  {
+    "revision": "d95ee50eb1677e5df1b8",
+    "url": "/pizza-creator/static/js/2.7d15bdeb.chunk.js"
+  },
+  {
+    "revision": "37ba4a1dc11123eaa106",
+    "url": "/pizza-creator/static/js/main.63224b21.chunk.js"
+  },
+  {
+    "revision": "6d95094391721a2850c6",
+    "url": "/pizza-creator/static/js/runtime~main.ea12da73.js"
+  },
+  {
+    "revision": "af8dad7aca34a61a61c16b257e3d791c",
+    "url": "/pizza-creator/static/media/error.af8dad7a.png"
+  }
+]);
